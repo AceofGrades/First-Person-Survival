59c81410371b8c206e0029e6382191abda1ae0d0
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2019-06-11 11:08:46 +1000

    Fixed script positioning, added pickup script

---
+++ Assets/Scripts/Environment/PickUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
