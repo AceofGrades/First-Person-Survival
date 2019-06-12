d54715dc86140f069d82b22c9d473cceab612af8
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2019-06-12 11:23:22 +1000

    Added sounds

--- Assets/Scripts/Environment/PickUp.cs
+++ Assets/Scripts/Environment/PickUp.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PickUp : MonoBehaviour
 {
     public float speed;
+    public AudioSource myFx;
+    public AudioClip cardFx;
 
     void Start()
     {
@@ -24,6 +26,7 @@ public class PickUp : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            AudioSource.PlayClipAtPoint(cardFx, transform.position, 1f);
         }
     }
 }
