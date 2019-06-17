8bcfdf205adb88dea7b12b3ea56a64503ffa8e08
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2019-06-17 10:16:19 +1000

    Update Crosshair.cs

--- Assets/Scripts/Crosshair.cs
+++ Assets/Scripts/Crosshair.cs
@@ -9,5 +9,6 @@ public class Crosshair : MonoBehaviour
     void OnGUI()
     {
             GUI.Label(new Rect(Screen.width / 2 - 45, Screen.height / 2 - 23, 100, 100), crosshair);
+        
     }
 }
